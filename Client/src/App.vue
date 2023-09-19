16d29127b0d3118ed83514d4f0509d8183c7386e
kin <kin@email.com>
2023-09-19 14:07:18 +0700

    create client

---
+++ Client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 class="text-3xl font-bold underline text-slate-400">Hello Vue js with Tailwind CSS</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
